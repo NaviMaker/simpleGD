7cca0ae3c884851548d61793e5e7656d754d5369
NaviMaker <navilandsite@gmail.com>
2014-03-12 20:36:11 +0100

    Added Basic Functions

--- simplegd.class.php
+++ simplegd.class.php
@@ -16,12 +16,74 @@ class simpleGD{
 	
 	public $width;
 	public $height;
-	public $format;
-	public $quality;
+	public $format 	= "jpg"; 		// jpg, png, gif
+	public $quality = 90; 			// 1-100 only for jpg
 	
-	public $resizeMode;
-	public $rotate;
+	public $resizeMode = "crop"; 	//crop, warp
+	public $rotate 	   = 0; 		//0, 90, 180, 270
 	
-	public $usecache;
+	public $resizeIfSmaller = false;
+	public $enableCache = false;
+	public $cachedir 	= "";
+	public $deleteOriginal = false;
+	
+	/* The following variables are for internal use only don't edit them! */
+	private $resourceImage;
+	private $originalWidth;
+	private $originalHeight;
+	
+	private function loadResource($resource)
+	{
+		if(!file_exists($resource))
+		{
+			throw new Exception ("simpleGD :: This resource does not exist");
+		}
+		else
+		{
+			$resource_ext = pathinfo($resource, PATHINFO_EXTENSION);
+			
+			if($resource_ext == "jpg")
+			{
+				$this->resourceImage = imagecreatefromjpeg($resource);
+			}
+			elseif($resource_ext == "png")
+			{
+				$this->resourceImage = imagecreatefrompng($resource);
+			}
+			elseif($resource_ext == "gif")
+			{
+				$this->resourceImage = imagecreatefromgif($resource);
+			}
+			else
+			{
+				throw new Exception ("simpleGD :: I can't handle this file type");
+			}
+		}
+	}
+	
+	public function render()
+	{
+		$this->loadResource($this->source);
+		$this->getSourceSize();
+		if(!$this->resizeIfSmaller)
+		{
+			if( $this->originalWidth < $this->width || $this->originalHeight < $this->height )
+			{
+				return saveResource();
+			}
+		}
+	}
+	
+	private function getSourceSize()
+	{
+		list($width, $height) = getimagesize($this->source);
+		$this->originalWidth  = $width;
+		$this->originalHeight = $height;
+	}
+	
+	private function saveResource()
+	{
+		
+	}
 }
 ?>
