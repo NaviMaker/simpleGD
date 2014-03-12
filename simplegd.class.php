d77da5d94c2d5a1776152203f9c84922ef9fdc31
NaviMaker <navilandsite@gmail.com>
2014-03-12 19:00:43 +0100

    First Vars

---
+++ simplegd.class.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ *  simpleGD by NaviMaker
+ *  https://github.com/NaviMaker/simpleGD
+ *
+ *	Licence: GPL V2
+ *  Description:
+ *	This is a simple PHP class I wrote to handle images 
+ *
+ */
+
+class simpleGD{
+
+	public $source;
+	public $destination;
+	
+	public $width;
+	public $height;
+	public $format;
+	public $quality;
+	
+	public $resizeMode;
+	public $rotate;
+	
+	public $usecache;
+}
+?>
